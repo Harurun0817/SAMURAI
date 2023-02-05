afa04bccdf5c8637a4cae1b255931d2b0519ceea
motoki kyan <kyan@ryukyuhub.co.jp>
2023-02-05 22:03:02 +0900

    トップとマーケットレポートページを修正した

---
+++ retail/js/common.js
@@ -0,0 +1,54 @@
+header();
+nav();
+sideMenu();
+footer();
+function header() {
+  fetch('components/header.html')
+    .then(res => res.text())
+    .then(data => {
+      const header = document.getElementsByTagName('header');
+      header[0].innerHTML = data;
+    });
+}
+function nav() {
+  fetch('components/nav.html')
+    .then(res => res.text())
+    .then(data => {
+      const nav = document.getElementsByTagName('nav');
+      nav[0].innerHTML = data;
+    });
+}
+function sideMenu() {
+  fetch('components/sidemenu.html')
+    .then(res => res.text())
+    .then(data => {
+      const sideMenu = document.getElementById('sideMenu');
+      sideMenu.innerHTML = data;
+    });
+}
+function footer() {
+  fetch('components/footer.html')
+    .then(res => res.text())
+    .then(data => {
+      const footer = document.getElementsByTagName('footer');
+      footer[0].innerHTML = data;
+    });
+}
+const x = 2;
+function getFontSize(){
+  const root = document.documentElement;
+  const style = window.getComputedStyle(root).getPropertyValue('font-size');
+  return parseFloat(style);
+}
+function normal(){
+  const fs = getFontSize();
+  document.documentElement.style.fontSize = '16px';
+}
+function larger(){
+  const fs = getFontSize();
+  document.documentElement.style.fontSize = (fs + x) + 'px';
+}
+function smaller(){
+  const fs = getFontSize();
+  document.documentElement.style.fontSize = (fs - x) + 'px';
+}
--- retail/js/top.js
+++ retail/js/top.js
@@ -1,5 +1,3 @@
-console.log("ok");
-
 // ボタン、モダル、モダルの閉じるボタン、オーバーレイを変数に格納
 const restate = document.querySelector('.restate');
 const modal = document.querySelector('.my_modal');
@@ -10,7 +8,6 @@ const overlay = document.querySelector('.overlay');
 // ボタンをクリックしたら、モダルとオーバーレイに.activeを付ける
 restate.addEventListener('click', function(e){
     // aタグのデフォルトの機能を停止する    
-    console.log("test");
     e.preventDefault();
     // モーダルとオーバーレイにactiveクラスを付与する
     modal.classList.add('active');
@@ -35,11 +32,3 @@ window.onload = function () {
     modal.classList.add('active');
     overlay.classList.add('active');
 };
-
-
-let x = 100;
-function normal()  { document.documentElement.style.fontSize = '100%'; }
-function larger()  { x *= 1.2; document.documentElement.style.fontSize = x + '%'; }
-function smaller() { x /= 1.2; document.documentElement.style.fontSize = x + '%'; }
-
-// let root = document.documentElement;
